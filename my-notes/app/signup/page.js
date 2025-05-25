afff7087286a98643241e30ccd71e9fa96950a44
geetharajan <43339136+geetharajan@users.noreply.github.com>
2025-05-26 04:12:31 +0530

    Create page.js

---
+++ my-notes/app/signup/page.js
@@ -0,0 +1,11 @@
+import AuthForm from '@/components/AuthForm';
+import Breadcrumbs from '@/components/Breadcrumbs';
+
+export default function SignupPage() {
+  return (
+    <>
+        {/* <Breadcrumbs/> */}
+        <AuthForm type="signup" />;
+    </>
+  ) 
+}
