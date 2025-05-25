ce6aa8a7c6edf20d52876a48faa1336a1e3293eb
geetharajan <43339136+geetharajan@users.noreply.github.com>
2025-05-26 04:03:57 +0530

    Create page.js

---
+++ my_notes/app/login/page.js
@@ -0,0 +1,10 @@
+import AuthForm from '@/components/AuthForm';
+
+export default function LoginPage() {
+  return (
+    <>
+        {/* <Breadcrumbs/> */}
+        <AuthForm type="login" />;
+    </>
+  )
+}
